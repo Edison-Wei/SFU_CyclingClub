9c225fdaa35c97e937af35591d4451e6d853ec32
Dreamz2 <Edwei395@gmail.com>
2024-02-14 12:14:38 -0800

    Fixed logo and routes

--- sfu_bikingclub/src/app/components/Navigation.js
+++ sfu_bikingclub/src/app/components/Navigation.js
@@ -1,12 +1,19 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Navigation() {
     return (
         <>
-           <div className="md:px-16"> 
-                    <a>
-                        <img src="./images/logo.jpg" alt= "logo" className={"md:px-4"} />
-                    </a>
+            <div className="md:px-16">
+                <a>
+                    <Image
+                        src={"/logo.jpg"}
+                        alt={"Picture of "}
+                        width={200}
+                        height={200}
+                        className="w-[30vw] h-[30vw] sm:w-[25vw] sm:h-[25vw] md:w-[15vw] md:h-[15vw] rounded-full object-cover shadow-md"
+                    />
+                </a>
             </div>
 
             <div className="md:px-10">
@@ -19,6 +26,6 @@ export default function Navigation() {
     )
 }
 
-function CreateLink({link, linkText}) {
+function CreateLink({ link, linkText }) {
     return <Link href={link} className="md:px-4">{linkText}</Link>
 }
