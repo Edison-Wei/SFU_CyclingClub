16140e9cb4ff506fb798416b12086f96dbe69ccb
cas32 <cas32@sfu.ca>
2024-03-01 11:26:42 -0800

    added routing to map

--- sfu_bikingclub/src/components/Map.js
+++ sfu_bikingclub/src/components/Map.js
@@ -1,15 +1,31 @@
+useIsClient
 import "../app/globals.css"
 import "leaflet/dist/leaflet.css";
 
-import { MapContainer , TileLayer} from "react-leaflet";
+import { MapContainer , TileLayer, useMap } from "react-leaflet";
+import L from "leaflet";
+import "leaflet-routing-machine";
+import { useIsClient } from "@uidotdev/usehooks";
 
 
 export default function Map() {
-
-    const markers = [
-
-
-    ]
+    // Define waypoints for the bike route
+    const waypoints = [
+        L.latLng(49.2732, -123.1000), 
+        L.latLng(49.2711, -123.1260), // Science World and False Creek Seawall
+    ];
+    
+    // Define a custom hook to add routing control to the map
+    function AddRoutingControl() {
+        const map = useMap();
+    
+        // Add routing control to the map
+        L.Routing.control({
+        waypoints: waypoints,
+        }).addTo(map);
+    
+        return null;
+    }
     
     return (
         <MapContainer center={[49.246292, -123.116226]} zoom={11.5}>
@@ -17,7 +33,7 @@ export default function Map() {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-        
+            <AddRoutingControl />
         </MapContainer>
 
     );
