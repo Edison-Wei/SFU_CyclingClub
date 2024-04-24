0834402b144c8686ffc69af794bd4c85ef3b2b2a
cas32 <claire_shou@sfu.ca>
2024-04-23 20:25:18 -0700

    removed the navigation and added lazy rendering for map

--- sfu_bikingclub/src/components/Map.js
+++ sfu_bikingclub/src/components/Map.js
@@ -1,46 +1,68 @@
-useIsClient
-import "../app/globals.css"
-import "leaflet/dist/leaflet.css";
 
-import { MapContainer , TileLayer, useMap } from "react-leaflet";
+import "../app/globals.css";
+import "leaflet/dist/leaflet.css";
+import { useMap } from "react-leaflet";
 import L from "leaflet";
 import "leaflet-routing-machine";
-import { useIsClient } from "@uidotdev/usehooks";
+import { useEffect } from "react";
+import dynamic from 'next/dynamic';
 
+// Lazy-loaded MapContainer for SSR
+const LazyMapContainer = dynamic(() => import('react-leaflet').then((module) => module.MapContainer), {
+  ssr: false
+});
 
-export default function Map() {
-    // Define waypoints for the bike route
-    const waypoints = [
-        L.latLng(49.2732, -123.1000), 
-        L.latLng(49.2711, -123.1260), // Science World and False Creek Seawall
-    ];
-    
-    // Define a custom hook to add routing control to the map
-    function AddRoutingControl() {
-        const map = useMap();
+// Lazy-loaded TileLayer for SSR
+const LazyTileLayer = dynamic(() => import('react-leaflet').then((module) => module.TileLayer), {
+  ssr: false
+});
 
-        L.marker([49.2732, -123.1000]).addTo(map)
+// Lazy-loaded Marker for SSR
+const LazyMarker = dynamic(() => import('react-leaflet').then((module) => module.Marker), {
+  ssr: false
+});
+
+function AddPopUp({ lat, long }) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (map) {
+      L.marker([lat, long])
+        .addTo(map)
         .bindPopup('Meet Here!')
         .openPopup();
-    
-        // Add routing control to the map
-        L.Routing.control({
-        waypoints: waypoints,
-        routeWhileDragging: true
-        }).addTo(map);
-    
-        return null;
     }
-    
-    return (
-        <MapContainer center={[49.246292, -123.116226]} zoom={11.5}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <AddRoutingControl />
-        </MapContainer>
+  }, [map, lat, long]);
 
-    );
+  return null;
+}
 
+function AddRoutingControl() {
+  const map = useMap();
+
+  useEffect(() => {
+    if (map) {
+      L.Routing.control({
+        waypoints: [
+          L.latLng(49.2732, -123.1000),
+          L.latLng(49.2711, -123.1260), // Science World and False Creek Seawall
+        ],
+        routeWhileDragging: true,
+      }).addTo(map);
+    }
+  }, [map]);
+
+  return null;
+}
+
+export default function Map() {
+  return (
+    <LazyMapContainer center={[49.246292, -123.116226]} zoom={11.5}>
+      <LazyTileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <AddPopUp lat={49.2732} long={-123.1000} />
+    </LazyMapContainer>
+  );
 }
