0da0f87a5fd71c6cd4f923d7ab8581a298671ff3
cas32 <claire_shou@sfu.ca>
2024-04-24 17:07:59 -0700

    changed the website title

--- sfu_bikingclub/src/components/Hero.js
+++ sfu_bikingclub/src/components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
         <nav className="relative">
             <img src={"https://www.sfu.ca/content/sfu/main/about/give/jcr:content/main_content/image.img.640.medium.jpg/1638826572368.jpg"} alt="Banner for the SFU Cycling Club" className="w-screen" />
             <div className="absolute w-[550px] md:inset-x-[25%] lg:inset-x-1/4 inset-y-1/2 md:text-[25px] lg:text-[32px] font-semibold text-white">
-                Welcome to SFU Cycling Club
+                SFU CYCLING CLUB
             </div>
             <div className="md:absolute md:w-full md:inset-y-0">
                 <Navigation />
