b0e7fdbcb0d9069282e3fa7279e1b1d7aeb1482d
Dreamz2 <Edwei395@gmail.com>
2024-04-25 15:15:17 -0700

    Deleted body component and moved it into the page file

--- sfu_bikingclub/src/app/page.js
+++ sfu_bikingclub/src/app/page.js
@@ -4,66 +4,55 @@ import Header from "../components/Header"
 import TextWithButton from "../components/TextWithButton";
 import { SmallText } from "../components/TextWithButton";
 import Link from "next/link";
-// import Map from "../components/Map";
 import { useMemo } from "react";
 import dynamic from "next/dynamic";
 
 
 export default function Home() {
-  const Map = useMemo(() => dynamic(
-    () => import('@/components/Map'),
-  {
-    loading: () => <p>Map is Loading</p>,
-  ssr: false
-    }
-  ), []);
 
-  // const Mappane = dynamic(import('@/components/Map'), {
-  //   ssr: false,
-  //   loading: <p>Map is Loading</p>
-  // })
+  const Mappane = dynamic(() => import('@/components/Map'), { ssr: false, loading: () => <p>Map is Loading</p> });
 
   // dark:invert-[.95] dark:hue-rotate-180
   return (
     <main className="font-mono">
       <Header />
       <Hero />
-        <div className="">
+      <div className="">
+        <div className="flex justify-around items-center md:h-[400px] lg:h-[600px] xl:h-[800px] bg-black text-white">
+          <div className="">
+            <TextWithButton title={"Upcoming Rides!"} text={"Come join us on any of our upcoming rides!"} stext={""} link={"https://www.strava.com/clubs/1079967"} linkName={"Strava"}></TextWithButton>
+          </div>
+
           <div className="flex justify-around items-center md:h-[400px] lg:h-[600px] xl:h-[800px] bg-black text-white">
             <div className="">
-              <TextWithButton title={"Upcoming Rides!"} text={"Come join us on any of our upcoming rides!"} stext={""} link={"https://www.strava.com/clubs/1079967"} linkName={"Strava"}></TextWithButton>
+              <SmallText stext={"Upcoming Ride"} />
+              <Mappane />
+              <CreateLink link={"./Suggestion"} linkText={"Make a Suggestion"} />
             </div>
-
-            <div className="flex justify-around items-center md:h-[400px] lg:h-[600px] xl:h-[800px] bg-black text-white">
-              <div className="">
-                <SmallText stext={"Upcoming Ride"} />
-                <Map />
-                <CreateLink link={"./Suggestion"} linkText={"Make a Suggestion"} />
-              </div>
-            </div>
-          </div>
-          <div className="flex justify-around items-center md:h-[400px] lg:h-[600px] xl:h-[800px]">
-            <div>
-              Previous Rides
-            </div>
-            <div>
-              Thing 2
-            </div>
-          </div>
-          <div className="flex justify-around md:h-[400px] lg:h-[600px] xl:h-[850px] items-center bg-black text-white">
-            <div className="w-1/2 h-1/2">
-              <img src={`https://hips.hearstapps.com/hmg-prod/images/coastal-cyclists-credit-bob-markisello-1575992964.jpg?crop=0.642xw:1.00xh;0.187xw,0&resize=980:*`}
-                alt={"Group photo of the Cycling Club (Once it happens)"}
-                className="h-auto w-auto" />
-            </div>
-            <p id="joinInformation" />
-            <TextWithButton title={"Join Our Riding Community"} text={"Hello World!"} stext={"Ready To Join the SFU Cycling Club?"} link={"https://www.sfu.ca/"} linkName={"Discord"} />
           </div>
         </div>
+        <div className="flex justify-around items-center md:h-[400px] lg:h-[600px] xl:h-[800px]">
+          <div>
+            Safety Information
+          </div>
+          <div>
+            Gear to bring on rides
+          </div>
+        </div>
+        <div className="flex justify-around md:h-[400px] lg:h-[600px] xl:h-[850px] items-center bg-black text-white">
+          <div className="w-1/2 h-1/2">
+            <img src={`https://hips.hearstapps.com/hmg-prod/images/coastal-cyclists-credit-bob-markisello-1575992964.jpg?crop=0.642xw:1.00xh;0.187xw,0&resize=980:*`}
+              alt={"Group photo of the Cycling Club (Once it happens)"}
+              className="h-auto w-auto" />
+          </div>
+          <p id="joinInformation" />
+          <TextWithButton title={"Join Our Riding Community"} text={"Hello World!"} stext={"Ready To Join the SFU Cycling Club?"} link={"https://www.sfu.ca/"} linkName={"Discord"} />
+        </div>
+      </div>
     </main>
   )
 }
 
-function CreateLink({link, linkText}) {
+function CreateLink({ link, linkText }) {
   return <Link href={link} className="md:px-4">{linkText}</Link>
 }
