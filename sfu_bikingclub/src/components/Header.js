8c307ad6637db2566fbdff4677edae152b301338
cas32 <claire_shou@sfu.ca>
2024-04-25 18:52:52 -0700

    added colour changing when hovering over links

--- sfu_bikingclub/src/components/Header.js
+++ sfu_bikingclub/src/components/Header.js
@@ -7,19 +7,19 @@ export default function Header() {
 
     return (
         <nav className="md:flex md:justify-between md:w-full">
-          <div className="flex-none w-96 bg-white-500 md:text-[20px] font-[550]">
+          <div className="flex-none w-96 bg-white-500 md:text-[20px] font-[550] hover:text-gray-500">
             <Link href="#joinInformation">JOIN US!</Link>
           </div>
           <div className="flex-1 md:px-10 text-[12px] md:text-[20px] font-[550] flex justify-end">
             <CreateLink link={"./About"} linkText={"About"} />
             <CreateLink link={"./Executives"} linkText={"Executives"} />
-            <CreateLink link={"./Blog"} linkText={"Cycling Blog"} />
-            <a href={"https://www.strava.com/clubs/1079967"} className={"md:px-4"}>Strava</a>
+            <CreateLink link={"./Blog"} linkText={"Blog"} />
+            <a href={"https://www.strava.com/clubs/1079967"} className={"md:px-4 hover:text-gray-500"}>Strava</a>
           </div>
         </nav>
       );
 }
 
 function CreateLink({ link, linkText }) {
-    return <Link href={link} className="md:px-4">{linkText}</Link>
+    return <Link href={link} className="md:px-4 hover:text-gray-500">{linkText}</Link>
 }
