91d41b592a222ae89d1fdf28530831a50b971534
cas32 <claire_shou@sfu.ca>
2024-04-26 18:47:56 -0700

    changed the spacing for the footer

--- sfu_bikingclub/src/components/Footer.js
+++ sfu_bikingclub/src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
 
     return (
-        <footer className="w-full bg-white">
+        <footer className="ml-4 w-full bg-white">
             <p>Made by Edison Wei and Claire Shou</p>
         </footer>
     )
