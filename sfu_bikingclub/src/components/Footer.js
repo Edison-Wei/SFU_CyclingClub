e73f68f572a1a541cd6a498c790fd2602ceb5cba
cas32 <claire_shou@sfu.ca>
2024-04-27 17:21:13 -0700

    changed Footer text colour

--- sfu_bikingclub/src/components/Footer.js
+++ sfu_bikingclub/src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
 
     return (
-        <footer className="ml-4 w-full bg-white">
+        <footer className="ml-4 w-full bg-white text-[#890B29]">
             <p>Made by Edison Wei and Claire Shou</p>
         </footer>
     )
