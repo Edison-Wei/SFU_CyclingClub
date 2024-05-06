db64c30da7eef3c038539ba913fbd61bb73f754a
cas32 <claire_shou@sfu.ca>
2024-05-05 23:15:26 -0700

    Sign-up form functionality done. Added mongoDB connection and APIs for fetching user data

---
+++ sfu_bikingclub/src/app/api/signup/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { connectMongoDB } from "../../../../lib/mongodb";
+import User from "../../../../models/user";
+import bcrypt from "bcryptjs";
+
+export async function POST(req) {
+    try {
+        const {name, email, password} = await req.json();
+        
+        // encrypt user password
+        const hasedPassword = await bcrypt.hash(password, 10);
+
+        // connecting and storing the data to MongoDB 
+        await connectMongoDB();
+        await User.create({name, email, password: hasedPassword});
+
+
+        return NextResponse.json({message: "User registered."}, {status: 201});
+
+    } catch(error) {
+        return NextResponse.json({message: "An error occured while registering the user"}, {status: 500});
+    }
+}
--- sfu_bikingclub/src/components/SignUpForm.js
+++ sfu_bikingclub/src/components/SignUpForm.js
@@ -1,16 +1,93 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function SignUpForm() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // user does not fill out all fields
+        if(!name || !email || !password) {
+            setError("All fields are necessary.");
+            return;
+        }
+
+        try {
+
+            // check if user exists
+            const resUserExists = await fetch("api/userExists", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({email})
+            });
+
+            const {user} = await resUserExists.json();
+
+            // set error if the user exists already
+            if (user) {
+                setError("User already exists.");
+                return;
+            }
+
+            const res = await fetch('api/signup', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    name, email, password
+                })
+            });
+
+            if(res.ok) {
+                const form = e.target;
+                form.reset();
+
+            } else {
+                console.log("User registration failed.");
+            }
+
+        } catch (error) {
+            console.log("Error during registration: ", error);
+        }
+    };
+
+    console.log("Name: ", name);
+
     return (
         <div className="grid place-items-center h-screen">
             <div className="shadow-lg p-5 border-t-4">
                 <h1 className="text-xl font-bold my-4">Sign Up</h1>
-                <form className="flex flex-col gap-3">
-                    <input type="text" placeholder="Full Name"/>
-                    <input type="text" placeholder="SFU Email"/>
-                    <input type="password" placeholder="Password"/>
+
+                <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+
+                    <input onChange={e => setName(e.target.value)} 
+                            type="text" 
+                            placeholder="Full Name"/>
+
+                    <input onChange={e => setEmail(e.target.value)}
+                            type="text" 
+                            placeholder="SFU Email"/>
+
+                    <input onChange={e => setPassword(e.target.value)}
+                            type="password" 
+                            placeholder="Password"/>
+
                     <button className="bg-[#890B29] text-white text-sm font-bold cursor-pointer px-6 py-2">SIGN UP</button>
-                    <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">Error Message</div>
+                    
+                    
+                    { error && (
+                        <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">{error}</div>)
+                    }
     
                     <Link className="text-sm mt-3 text-right" href={'/blog/login'}>
                         Already have an account? <span className="underline">Login Here</span>
@@ -18,5 +95,5 @@ export default function SignUpForm() {
                 </form>
             </div>
         </div>
-        );
+    );
 }
