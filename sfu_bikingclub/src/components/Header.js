289601c49875c83b7f4792d510d462fad778e609
cas32 <claire_shou@sfu.ca>
2024-08-09 01:53:02 -0700

    capitalized header sections to look more visually appealing

--- sfu_bikingclub/src/components/Header.js
+++ sfu_bikingclub/src/components/Header.js
@@ -6,11 +6,11 @@ export default function Header() {
   return (
     <nav className="md:flex md:justify-between md:w-full sticky top-0 left-0 w-full z-10">
       <div className="flex-1 bg-primary-red md:px-10 md:text-[15px] font-[550] text-white flex justify-around md:justify-end">
-        <CreateLink link={"/"} linkText={"Home"} />
-        <CreateLink link={"/About"} linkText={"About"} />
-        <CreateLink link={"/Executives"} linkText={"Executives"} />
-        <CreateLink link={"/blog"} linkText={"Blog"} />
-        <a href={"https://www.strava.com/clubs/1079967"} className={"md:px-4 hover:text-gray-500"}>Strava</a>
+        <CreateLink link={"/"} linkText={"HOME"} />
+        <CreateLink link={"/About"} linkText={"ABOUT"} />
+        <CreateLink link={"/Executives"} linkText={"EXECUTIVES"} />
+        <CreateLink link={"/blog"} linkText={"BLOG"} />
+        <a href={"https://www.strava.com/clubs/1079967"} className={"md:px-4 hover:text-gray-500"}>STRAVA</a>
       </div>
     </nav>
   );
