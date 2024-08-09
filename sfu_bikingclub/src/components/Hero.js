256382a408ff76d6126153382a1538c7d1e55805
cas32 <claire_shou@sfu.ca>
2024-08-09 10:01:34 -0700

    made the SFU CYCLING CLUB title easier to read and see

--- sfu_bikingclub/src/components/Hero.js
+++ sfu_bikingclub/src/components/Hero.js
@@ -7,21 +7,21 @@ export default function Hero() {
             <img
                 src={"banner.jpg"}
                 alt="Banner for the SFU Cycling Club"
-                className="w-full h-[50vw] object-cover"
+                className="w-full h-[50vw]  object-cover"
             />
-            <div className="absolute flex items-center w-[90%] sm:w-[550px] md:w-[550px] lg:w-[550px] inset-x-[5%] sm:inset-x-[25%] md:inset-x-1/4 inset-y-1/2 transform -translate-y-1/2 text-white">
+            <div className="absolute flex items-center w-[90%] sm:w-[550px] md:w-[650px] lg:w-[750px] inset-x-[5%] sm:inset-x-[25%] md:inset-x-1/4 inset-y-1/2 transform -translate-y-1/2 text-white z-10">
                 <Link href="./">
                     <img
                         src={"/logo.jpg"}
                         alt={"Picture of SFU Cycling Club Logo"}
-                        className="w-[30vw] h-[30vw] sm:w-[20vw] sm:h-[20vw] md:w-[10vw] md:h-[10vw] rounded-full object-cover shadow-md"
+                        className="w-[30vw] h-[30vw] sm:w-[20vw] sm:h-[20vw] md:w-[15vw] md:h-[15vw] lg:w-[10vw] lg:h-[10vw] rounded-full object-cover shadow-md"
                     />
                 </Link>
-                <div className="ml-4">
-                    <p className="mb-2 text-[6vw] sm:text-[40px] md:text-[40px] lg:text-[40px] font-semibold">
+                <div className="ml-4 flex flex-col justify-center">
+                    <p className="text-[10vw] sm:text-[40px] md:text-[50px] lg:text-[60px] font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] whitespace-nowrap overflow-hidden text-ellipsis">
                         SFU CYCLING CLUB
                     </p>
-                    <div className="text-[3vw] sm:text-[10px] md:text-[15px] lg:text-[15px] font-[550] hover:text-rose-600">
+                    <div className="text-[3vw] sm:text-[10px] md:text-[15px] lg:text-[15px] font-[550] hover:text-rose-600 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
                         <Link href="https://linktr.ee/sfucycling">JOIN NOW!</Link>
                     </div>
                 </div>
