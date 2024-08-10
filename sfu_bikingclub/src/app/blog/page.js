85907af6fe4e6b98d788f058df06fec879c97043
cas32 <claire_shou@sfu.ca>
2024-08-09 21:53:37 -0700

    added text for blog page

--- sfu_bikingclub/src/app/blog/page.js
+++ sfu_bikingclub/src/app/blog/page.js
@@ -8,12 +8,15 @@ export default function Blog() {
         <div className="w-full h-full py-2">
             <Header />
             {/* <UserInfo /> */}
-            <div className="flex flex-wrap justify-center">
+            {/* <div className="flex flex-wrap justify-center"> */}
+            <div className="w-full h-screen flex flex-col justify-center items-center bg-white text-black">
+                <h1 className="text-4xl md:text-6xl font-bold mb-4">No blog posts yet!</h1>
+                <p className="text-lg md:text-xl">Stay tuned for updates!</p>
+                {/* <BlogPost />
                 <BlogPost />
                 <BlogPost />
                 <BlogPost />
-                <BlogPost />
-                <BlogPost />
+                <BlogPost /> */}
                 {/* Add more BlogPost components here as needed */}
             </div>
             <div className="sticky bottom-0 flex justify-center w-full">
