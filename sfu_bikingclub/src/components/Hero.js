75acf185ac76353f36b9ef54c91b541e54f0cd62
cas32 <claire_shou@sfu.ca>
2024-08-11 23:44:46 -0700

    fixed logo on smaller screens

--- sfu_bikingclub/src/components/Hero.js
+++ sfu_bikingclub/src/components/Hero.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Hero() {
     return (
-        <nav className="relative flex items-center justify-center">
+        <nav className="relative justify-center">
             <img
                 src={"banner.jpg"}
                 alt="Banner for the SFU Cycling Club"
@@ -14,11 +14,11 @@ export default function Hero() {
                     <img
                         src={"/logo.jpg"}
                         alt={"Picture of SFU Cycling Club Logo"}
-                        className="w-[30vw] h-[30vw] sm:w-[20vw] sm:h-[20vw] md:w-[15vw] md:h-[15vw] lg:w-[10vw] lg:h-[10vw] rounded-full object-cover shadow-md"
+                        className="w-[20vw] h-[20vw] sm:w-[10vw] sm:h-[10vw] md:w-[10vw] md:h-[10vw] lg:w-[10vw] lg:h-[10vw] rounded-full object-cover shadow-md"
                     />
                 </Link>
                 <div className="ml-4 flex flex-col justify-center">
-                    <p className="text-[10vw] sm:text-[40px] md:text-[50px] lg:text-[60px] font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] whitespace-nowrap overflow-hidden text-ellipsis">
+                    <p className="text-[20px] sm:text-[40px] md:text-[50px] lg:text-[60px] font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] whitespace-nowrap overflow-hidden text-ellipsis">
                         SFU CYCLING CLUB
                     </p>
                     <div className="text-[3vw] sm:text-[10px] md:text-[15px] lg:text-[15px] font-[550] hover:text-rose-600 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
